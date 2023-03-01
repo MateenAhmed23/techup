4e84e5e97a88acbd598d1ccf66c4775112a66d15
MateenAhmed23 <mateen462@gmail.com>
2023-03-01 09:06:34 +0500

    added login signup api

--- server/index.js
+++ server/index.js
@@ -32,6 +32,63 @@ const saltRounds = parseInt(process.env.SALT_ROUNDS);
 mongoose.connect("mongodb://localhost:27017/techup");
 
 
+app.post("/api/register", (req, res) => {
+  try {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          status: "error",
+          message: "Error while Saving",
+        });
+      } else {
+        User.create({
+          name: req.body.username,
+          email: req.body.email,
+          password: hash,
+          role: req.body.role
+        });
+        return res.json({ status: "ok" });
+      }
+    });
+  } catch (err) {
+    return res.json({ status: "error", error: "duplicate email" });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email,
+  });
+  if (user) {
+    bcrypt.compare(req.body.password, user.password, function (err, result) {
+      if (result) {
+        const token = jwt.sign(
+          {
+            email: user.email,
+          },
+          process.env.JWT_SECRET,
+          { expiresIn: "20h" }
+        );
+        return res.json({
+          status: "ok",
+          token: token,
+          role: user.role,
+        });
+      } else {
+        return res.json({
+          status: "error",
+          message: "Email or password incorrect",
+        });
+      }
+    });
+  } else {
+    return res.json({ status: "error", message: "Email or password incorrect" });
+  }
+});
+
+
+
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
   });
