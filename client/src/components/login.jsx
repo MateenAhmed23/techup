6a64ef5c116d7aca25527d29642d08b463037940
MateenAhmed23 <mateen462@gmail.com>
2023-03-01 16:39:59 +0500

    added login signup logic in frontend

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -4,10 +4,46 @@ import Footer from "./subcomponents/footer";
 import Navbar from "./subcomponents/navbar";
 import LoginForm from "./subcomponents/loginform";
 
+
+
+
+
+// ERROR:
+
+
+// BACKEND NEEDS EMAIL, FRONTEND IS PROVIDING USERNAME
+
 class Login extends Component {
   state = {};
   handleLogin = (username, password) => {
     //Please enter login logic here @Mateen @Ahsan
+
+
+    const response = await fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+      if (data.status === "error"){
+        setError(true);
+        setErrorMsg(data.message);
+        setTimeout(() => {
+          setErrorMsg("");
+          setError(false);
+        }, 5000);
+        return;
+      }else{
+        localStorage.setItem("token", data.token);
+        // Redirect to the homepage
+        // history.push("/");
+      }
     console.log(username);
   };
 
--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -4,10 +4,53 @@ import Footer from "./subcomponents/footer";
 import Navbar from "./subcomponents/navbar";
 import SignupForm from './subcomponents/signupform';
 
+
+
+// ERROR:
+
+// WE ARE NOT CONFIRMING IF THE PASSWORD AND CONFIRMPASSWORD ARE THE SAME
+
+
 class Signup extends Component {
     state = {  } 
     handleSignup = (username, password) => {
         //Please enter login logic here @Mateen @Ahsan
+
+
+        if (password !== confirmPassword) {
+          setError(true);
+          setErrorMsg("Passwords do not match");
+          setTimeout(() => {
+            setErrorMsg("");
+            setError(false);
+          }, 5000);
+          return;
+        } else {
+          const response = await fetch("http://localhost:5000/api/register", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username,
+              email,
+              password,
+            }),
+          });
+    
+          const data = await response.json();
+          if (data.status === "error"){
+            setError(true);
+            setErrorMsg(data.message);
+            setTimeout(() => {
+              setErrorMsg("");
+              setError(false);
+            }, 5000);
+            return;
+          }else{
+            history.push("/login");
+          }
+        }
         console.log(username);
       };
     
