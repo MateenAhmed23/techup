92f4259ca7078400001b63532712aaa99adb4c1a
unknown <mateen462@gmail.com>
2023-03-03 12:44:26 +0500

    Changed signup component to functional

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import "./cssmaincomponents/login.css";
-import Footer from "./subcomponents/footer";
+// import Footer from "./subcomponents/footer";
 import Navbar from "./subcomponents/navbar";
 import SignupForm from './subcomponents/signupform';
 
@@ -11,63 +11,83 @@ import SignupForm from './subcomponents/signupform';
 // WE ARE NOT CONFIRMING IF THE PASSWORD AND CONFIRMPASSWORD ARE THE SAME
 
 
-class Signup extends Component {
-    state = {  } 
-    handleSignup = (username, password) => {
-        //Please enter login logic here @Mateen @Ahsan
+// class Signup extends Component {
+//     state = {  } 
+//     handleSignup = (username, password) => {
+//         //Please enter login logic here @Mateen @Ahsan
 
 
-        // if (password !== confirmPassword) {
-        //   setError(true);
-        //   setErrorMsg("Passwords do not match");
-        //   setTimeout(() => {
-        //     setErrorMsg("");
-        //     setError(false);
-        //   }, 5000);
-        //   return;
-        // } else {
-        //   const response = await fetch("http://localhost:5000/api/register", {
-        //     method: "POST",
-        //     headers: {
-        //       "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify({
-        //       username,
-        //       email,
-        //       password,
-        //     }),
-        //   });
+//         // if (password !== confirmPassword) {
+//         //   setError(true);
+//         //   setErrorMsg("Passwords do not match");
+//         //   setTimeout(() => {
+//         //     setErrorMsg("");
+//         //     setError(false);
+//         //   }, 5000);
+//         //   return;
+//         // } else {
+//         //   const response = await fetch("http://localhost:5000/api/register", {
+//         //     method: "POST",
+//         //     headers: {
+//         //       "Content-Type": "application/json",
+//         //     },
+//         //     body: JSON.stringify({
+//         //       username,
+//         //       email,
+//         //       password,
+//         //     }),
+//         //   });
     
-        //   const data = await response.json();
-        //   if (data.status === "error"){
-        //     setError(true);
-        //     setErrorMsg(data.message);
-        //     setTimeout(() => {
-        //       setErrorMsg("");
-        //       setError(false);
-        //     }, 5000);
-        //     return;
-        //   }else{
-        //     history.push("/login");
-        //   }
-        // }
-        console.log(username);
-      };
+//         //   const data = await response.json();
+//         //   if (data.status === "error"){
+//         //     setError(true);
+//         //     setErrorMsg(data.message);
+//         //     setTimeout(() => {
+//         //       setErrorMsg("");
+//         //       setError(false);
+//         //     }, 5000);
+//         //     return;
+//         //   }else{
+//         //     history.push("/login");
+//         //   }
+//         // }
+//         console.log(username);
+//       };
     
-      render() {
-        return (
-          <div className="loginpage">
+//       render() {
+//         return (
+//           <div className="loginpage">
+//             <Navbar onnavclick={this.handleclick} className="navbar"></Navbar>
+    
+//             <div className="LogForm">
+//               <SignupForm onSubmit={this.state.handleSignup} />
+//             </div>
+//             <div className="footer">
+//               <h1 className="heading">Footer</h1>
+//             </div>
+//           </div>
+//         );
+//       }
+//     }
+
+
+function Signup(){
+
+  function handleSignup(username,email,password,confirmPassword){
+    console.log('Inside Signup Handle Function')
+  }
+  return (
+    <div className="loginpage">
             <Navbar onnavclick={this.handleclick} className="navbar"></Navbar>
     
             <div className="LogForm">
-              <SignupForm onSubmit={this.state.handleSignup} />
+              <SignupForm onSubmit={handleSignup} />
             </div>
             <div className="footer">
               <h1 className="heading">Footer</h1>
             </div>
           </div>
-        );
-      }
-    }
+  )
+}
  
 export default Signup;
