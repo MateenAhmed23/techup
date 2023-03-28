6b68506bfdd0699ea285774b804a4bf3181eadcb
unknown <mateen462@gmail.com>
2023-03-28 14:42:33 +0500

    Implemented navigation and token functionality in login page

--- client/src/components/helperFunction/verifyToken.js
+++ client/src/components/helperFunction/verifyToken.js
@@ -5,37 +5,31 @@ async function verifyToken(token) {
     console.log('Inside Verify Token function')
 //   const [isValid, setIsValid] = useState(false);
 
-    var isValid = false
 
-
-    try {
-        await fetch('http://127.0.0.1:5000/api/verify-token', {
+    try{
+        const res = await fetch('http://127.0.0.1:5000/api/verify-token', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
             'Authorization': token // Include the token in the Authorization header
           }
-        }).then(res=>res.json()).then(data=>{
-            console.log(data.payload.email)
-
-            return {
-                userID: data.payload.userID,
-                verify: true
-            }
         })
+        // console.log('HAHA')
+        const r = await res.json()
 
-        // console.log(response, 'RESPONSE FROM VERIFY TOKEN');
-      } catch (error) {
-        console.error(error,'ERROR IN VERIFY TOKEN');
-        return {
-            verify:false
+        // console.log(r)
+        // console.log(r.payload.userId)
+
+        return{
+          valid: r.valid,
+          _id: r.payload.userId
+        }
+        // console.log(r)
+      }catch(e){
+        return{
+          valid: false
         }
       }
-
-
-  return {
-
-  };
 }
 
 export default verifyToken;
