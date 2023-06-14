3225b1c8ef864130721149d380a130eaff38cb09
unknown <mateen462@gmail.com>
2023-06-14 09:09:29 +0500

    Added flow of App

--- client/src/components/subcomponents/companyNav.jsx
+++ client/src/components/subcomponents/companyNav.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import "./csssubcomponents/companyNav.css";
 import logout from "./csssubcomponents/logout.png";
 import dropdown from"./csssubcomponents/dropdown.png";
@@ -6,46 +6,53 @@ import notifications from"./csssubcomponents/BELL.png";
 
 import { useNavigate  } from 'react-router-dom';
 
+import UserContext from "../../context/user";
+
 
 function CompNav(){
 
   const navigate = useNavigate();
 
+  const { userInfo, signOutUser } =
+    useContext(UserContext);
 
+    const [companyName, setCompanyName] = useState('')
     function signoutUser(){
-        localStorage.removeItem("token");
-        navigate('/login')
+        signOutUser();
+        navigate('/');
     } 
-    
+
+    async function getCompanyName(){
+    const res = await fetch("http://127.0.0.1:5000/api/get-user-info", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: userInfo.companyId,
+      }),
+    });
+
+    const data = await res.json();
+
+    setCompanyName(data.name)
+}
+
+useEffect(()=>{
+    getCompanyName();
+}, [companyName])
+
     return(
         <nav>
                 <div className="nav-left">
                     <img src={dropdown} className="logo" alt="logo" />
-                    <p className="companyname">Company Name</p>
+                    <p className="companyname">{companyName}</p>
                 </div>
                 <div className="nav-right">
-                    <img src={notifications} className="bellicon" alt="notifications" />
-                    <img src={logout} className="avatar" alt="logout" onClick={()=>signoutUser()}/>
+                    <img src={logout} className="avatar" alt="logout" onClick={()=>signoutUser()}/> 
                 </div>
             </nav>
     )
 }
-// class CompNav extends Component {
-//     state = {  } 
-//     render() { 
-//         return (
-//             <nav>
-//                 <div className="nav-left">
-//                     <img src={dropdown} className="logo" alt="logo" />
-//                     <p className="companyname">Company Name</p>
-//                 </div>
-//                 <div className="nav-right">
-//                     <img src={notifications} className="bellicon" alt="notifications" />
-//                     <img src={logout} className="avatar" alt="logout" onClick={()=>signoutUser()}/>
-//                 </div>
-//             </nav>
-//         );
-//     }
-// }
  
 export default CompNav;
